a5ea60f6613ce33265be6d62b3be0cbe8dff3459
EmmanuelBeja <beja.emmanuel@gmail.com>
2018-09-07 20:22:30 +0300

    [finishes #160341465] View all orders

---
+++ tests.py
@@ -0,0 +1,30 @@
+"""
+tests.py
+"""
+import unittest
+from app import create_app
+
+class OrderTestCase(unittest.TestCase):
+    """This class represents the order test case"""
+
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app(CONFIGNAME="testing")
+        self.client = self.app.test_client
+        self.order = {
+            "id": 1,
+            "food_name": "Pilau",
+            "food_price": 350,
+            "client_name": "Frank Oliel",
+            "client_adress": "Nyali"
+        }
+
+    def test_api_can_get_all_orders(self):
+        """Test API can get all orders (GET request)."""
+        res = self.client().get('/v1/orders/')
+        self.assertEqual(res.status_code, 200)
+
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+    unittest.main()
