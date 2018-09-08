491a882f00267f308189b4c6fc63ee544d05efc2
EmmanuelBeja <beja.emmanuel@gmail.com>
2018-09-08 23:21:40 +0300

    [finishes #160341465-allorders] fix travis ci build

--- run.py
+++ run.py
@@ -5,9 +5,9 @@ import os
 
 from app import create_app
 
-#config_name = os.getenv('APP_SETTINGS') # 
+#config_name = os.getenv('APP_SETTINGS') #
 config_name = "development"
 app = create_app(config_name)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
