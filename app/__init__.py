f9807d918953752943a55bb4ddd2cbb9f9a09a1e
EmmanuelBeja <beja.emmanuel@gmail.com>
2018-09-10 00:41:10 +0300

    [finishes #160341506] manipulating orders; fetch, edit delete

--- app/__init__.py
+++ app/__init__.py
@@ -12,19 +12,67 @@ def create_app(config_name):
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
 
-    @app.route('/v1/orders/', methods=['GET'])
-    def orders():
-        # GET
+    @app.route('/v1/orders/<int:order_id>', methods=['GET', 'PUT', 'DELETE'])
+    def order_manipulation(order_id, **kwargs):
+        # retrieve an order using it's order_id
+        #get all orders first
         orders = Order.get_all()
-        results = []
-
+        #filter through to get specific order
         for index in range(len(orders)):
-            for key in orders[index]:
-                print(orders[index][key])
-                obj = orders[index][key]
-                results.append(obj)
-        response = jsonify(results)
-        response.status_code = 200
-        return response
+            if orders[index]['order_id'] == order_id:
+                order = orders[index]
+
+        if not order:
+            # Raise an HTTPException with a 404 not found status code
+            abort(404)
+
+        if request.method == 'DELETE':
+            order = Order(order_id=order_id, food_name='', food_price='', client_name='', client_adress='')
+            orders = order.delete()
+
+            results = []
+
+            for index in range(len(orders)):
+                for key in orders[index]:
+                    print(orders[index][key])
+                    obj = orders[index][key]
+                    results.append(obj)
+
+            message = {
+            "message": "order {} deleted successfully".format(order.order_id)
+            }
+            print(message)
+
+            response = jsonify(results)
+            response.status_code = 200
+            return response
+
+        elif request.method == 'PUT':
+            food_name = str(request.data.get('food_name'))
+            food_price = str(request.data.get('food_price'))
+            client_name = str(request.data.get('client_name'))
+            client_adress = str(request.data.get('client_adress'))
+
+            order['food_name'] = food_name
+            order['food_price'] = food_price
+            order['client_name'] = client_name
+            order['client_adress'] = client_adress
+
+            order = Order(order_id=order_id, food_name=order['food_name'], food_price=order['food_price'], client_name=order['client_name'], client_adress=order['client_adress'])
+            result = order.save()
+            response = jsonify(result)
+            response.status_code = 200
+            return response
+        else:
+            # GET
+            response = jsonify({
+                'order_id': order['order_id'],
+                'food_name': order['food_name'],
+                'food_price': order['food_price'],
+                'client_name': order['client_name'],
+                'client_adress': order['client_adress']
+            })
+            response.status_code = 200
+            return response
 
     return app
--- tests.py
+++ tests.py
@@ -21,11 +21,27 @@ class OrderTestCase(unittest.TestCase):
             "client_adress": "Nyali"
         }
 
-    def test_api_can_get_all_orders(self):
-        """Test API can get all orders (GET request)."""
-        res = self.client().get('/v1/orders/')
-        self.assertEqual(res.status_code, 200)
+    def test_api_can_get_order_by_id(self):
+        """Test API can get a single order by using it's order_id."""
+        rv = self.client().get('/v1/orders/1')
+        self.assertEqual(rv.status_code, 200)
 
+    def test_order_can_be_edited(self):
+        """Test API can edit an existing order. (PUT request)"""
+        rv = self.client().put(
+            '/v1/orders/1',
+            data={
+                "food_name": "French Fries",
+                "food_price": 150,
+                "client_name": "Logan Wolverine",
+                "client_adress": "Watamu, Kenya"
+            })
+        self.assertEqual(rv.status_code, 200)
+
+    def test_order_deletion(self):
+        """Test API can delete an existing order. (DELETE request)."""
+        res = self.client().delete('/v1/orders/1')
+        self.assertEqual(res.status_code, 200)
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
