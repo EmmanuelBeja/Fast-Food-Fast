bc5f4a0557c3a11cac734a4fbcc85a9ea81b7f72
EmmanuelBeja <beja.emmanuel@gmail.com>
2018-09-09 13:28:48 +0300

    [finishes #160341546] order creation endpoint. fixed pep8 error

--- tests.py
+++ tests.py
@@ -14,16 +14,12 @@ class OrderTestCase(unittest.TestCase):
         self.app = create_app(config_name="testing")
         self.client = self.app.test_client
 
-
     def test_order_creation(self):
         """Test API can create a order (POST request)"""
         requestdata1 = '?order_id=5&food_name=Pizza&food_price=1000'
         requestdata2 = '&client_name=Waititu&client_adress=Kwale'
         res = self.client().post('/v1/orders'+requestdata1+requestdata2)
         self.assertEqual(res.status_code, 201)
-
-
-
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
